05825bbb5d057a9698bf0b9617eb65ebdbc40c70
pouriapourrashidi <p.pourrashidy@gmail.com>
2022-08-15 15:28:00 -0400

    Updates

---
+++ precache-manifest.66df9786b8a9ce486343c8f2e8ffeef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a068e67cc60355f022d",
+    "url": "/Robots/static/css/main.a49d7e59.chunk.css"
+  },
+  {
+    "revision": "2a068e67cc60355f022d",
+    "url": "/Robots/static/js/main.2a068e67.chunk.js"
+  },
+  {
+    "revision": "23805d8f6a9c7d13c0f5",
+    "url": "/Robots/static/css/1.7bfe95e6.chunk.css"
+  },
+  {
+    "revision": "23805d8f6a9c7d13c0f5",
+    "url": "/Robots/static/js/1.23805d8f.chunk.js"
+  },
+  {
+    "revision": "a981b252f01dd12fb4fa",
+    "url": "/Robots/static/js/runtime~main.a981b252.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d7bc50d6fe66bd0ed3e1bc8339d5c399",
+    "url": "/Robots/index.html"
+  }
+];
