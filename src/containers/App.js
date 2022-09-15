a396de8412bb1606cd186de2467fcd8cfeb52ffd
pouriapourrashidi <p.pourrashidy@gmail.com>
2022-09-15 11:57:15 -0400

    Adding Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function APP() {
+  // This line has been removed so the HOOks can be added
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: ''
+  //   }
+  // }
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   
+  // }
+ 
+  
+
+  const [robots, setRobots]=useState([])
+  const [searchfield, setSearchfield]=useState('')
+  useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+        .then(response=> response.json())
+        .then(users => {setRobots(users)});
+  },[])
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
 
-  render() {
-    const { robots, searchfield } = this.state;
+    //const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
@@ -33,13 +41,12 @@ class App extends Component {
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
         </div>
       );
-  }
 }
 
-export default App;
+export default APP;
